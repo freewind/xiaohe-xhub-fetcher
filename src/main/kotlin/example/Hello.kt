ac5ab598c02abde092164c26ebc5bbf5952150c5
freewind <nowindlee@gmail.com>
2018-08-01 14:45:03 +0800

    can fetch

--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,7 +1,18 @@
 package example
 
+import com.beust.klaxon.JsonArray
+import com.beust.klaxon.Klaxon
+import com.github.kittinunf.fuel.Fuel
+
 fun main(args: Array<String>) {
-    println(hello("Kotlin"))
+    println(fetchFromXPub("小鹤双拼维护".toList()))
 }
 
-fun hello(name: String): String = "Hello, $name!"
+fun fetchFromXPub(chars: List<Char>): List<String> {
+    val result = Fuel.post("http://www.xhup.club/index.php/Xhup/Search/searchCode", listOf("search_word" to chars.joinToString(""))).responseString().third.get()
+    println(result)
+    val json = Klaxon().parseJsonObject(result.reader())
+    return json.array<JsonArray<String>>("list_dz")!!.map {
+        it.first().trim().split("""\s+""".toRegex()).last().replace("*", "")
+    }
+}
