d132b826b5478a9160158852aa099cc3c9755736
freewind <nowindlee@gmail.com>
2018-08-01 14:55:14 +0800

    fix some cases

--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -5,7 +5,11 @@ import com.beust.klaxon.Klaxon
 import com.github.kittinunf.fuel.Fuel
 
 fun main(args: Array<String>) {
-    println(fetchFromXPub("小鹤双拼维护".toList()))
+    val word = "小鹤双拼维护".toList()
+    val encode = fetchFromXPub(word)
+    word.zip(encode).forEach { (char, code) ->
+        println("$char: $code")
+    }
 }
 
 fun fetchFromXPub(chars: List<Char>): List<String> {
@@ -13,6 +17,10 @@ fun fetchFromXPub(chars: List<Char>): List<String> {
     println(result)
     val json = Klaxon().parseJsonObject(result.reader())
     return json.array<JsonArray<String>>("list_dz")!!.map {
-        it.first().trim().split("""\s+""".toRegex()).last().replace("*", "")
+        it.first().substringAfterLast("\u3000")
+                .trim()
+                .split("""\s+""".toRegex())
+                .last()
+                .replace("*", "")
     }
 }
